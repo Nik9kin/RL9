a38a3e50c566cd041b4e80426ccc54da39b8715b
Nik9kin <nik9kin01@gmail.com>
2025-03-06 22:56:57 +0300

    Beautiful __str__ for ConnectFour

--- game/games/connectfour.py
+++ game/games/connectfour.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from numbers import Integral
-from typing import Collection
+from typing import Collection, Iterable
 
 import numpy as np
 
@@ -11,6 +11,38 @@ from .kinrow import KInARowGameState, KInARowGame
 @dataclass(eq=False)
 class ConnectFourState(KInARowGameState):
     k: int = 4
+
+    def __str__(self) -> str:
+        rows, cols = self.grid.shape
+        if cols >= 100:
+            return super().__str__()
+
+        if cols <= 10:
+            col_ticks_func = lambda n: f'{n:<2d}'
+            n_col_ticks = cols
+            arr_sign = 'v '
+        else:
+            col_ticks_func = lambda n: f'{n:<2d}' if (n % 2) == 0 else '  '
+            n_col_ticks = (cols + 1) // 2
+            arr_sign = 'v   '
+
+        col_ticks = list(map(col_ticks_func, range(cols)))
+
+        first_row = ' ' + ''.join(col_ticks) + '\n'
+        second_row = ' ' + arr_sign * n_col_ticks + '\n'
+
+        str_mapping = {0: ' ', 1: 'x', 2: 'o'}
+        str_mapping_last_row = {0: '_', 1: 'x', 2: 'o'}
+        return (
+            first_row +
+            second_row +
+            '\n'.join(self._join('|', (str_mapping[i] for i in row)) for row in self.grid[:-1]) +
+            '\n' + self._join('|', (str_mapping_last_row[i] for i in self.grid[-1])) + '\n'
+        )
+
+    @staticmethod
+    def _join(sep: str, iterable: Iterable[str]):
+        return sep + sep.join(iterable) + sep
 
     def next(self, action: Integral | Collection[Integral]):
         if isinstance(action, Integral):
